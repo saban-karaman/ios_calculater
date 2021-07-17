ecb17cae2e7464779268fb32903df53272b6222d
saban-karaman <karaman.saban@gmail.com>
2021-07-17 11:00:29 +0300

    start

---
+++ index.js
@@ -0,0 +1,12 @@
+var b1 = parseInt(document.getElementById("b1").innerText);
+console.log(typeof (b1));
+result = parseInt(document.getElementById("row4").innerText);
+console.log(result);
+function abc(){
+    //document.addEventListener("click",
+      //  function () {
+            document.getElementById("row4").innerText = b1;
+        //}
+    //);
+    
+}
